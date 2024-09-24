cf44554dba7669df3f39b1a53f08ddb1efa4fd0d
Noah Ponto <noahpon@hotmail.com>
2024-09-23 18:37:45 -0700

    simulate

--- temp_controller/temp_controller.ino
+++ temp_controller/temp_controller.ino
@@ -1,4 +1,5 @@
 #ifndef TESTING
+#include <Adafruit_SoftServo.h>
 #include <Arduino.h>
 
 #include "hardware.h"
@@ -6,60 +7,75 @@
 #include "../tests/mocks.h"
 #endif
 
-const unsigned char maxFaucet = 255;
-const unsigned long samplePeriod = 200;  // Delay in ms between updates
+#define MOTOR_DISABLE_DELAY 3000  // Time in ms before disabling the motor
+#define SAMPLE_PERIOD 200         // Delay in ms between updates
+#define MAX_FAUCET 255            // Max value for the faucet
+#define OUPUT_NOISE_FILTER 10     // Amount of output noise to ignore
+
+unsigned long lastSampleTime = 0;
+unsigned long lastMotorUpdateTime = 0;
+
+uint8_t curFaucet, goalFaucet;
+uint8_t curTemp, goalTemp;
 
 // PID constants
 double kp = 5;
 double ki = 0;
 double kd = 0;
 
-unsigned char goalTemp = 90;
-unsigned char currentFaucet;
-
-unsigned long currentTime, previousTime;
+unsigned long curTime, prevTime;
 double elapsedTime;
 double error;
 double lastError;
 double input, output;
 double rateError, cumError;
 
-void computePID(double currentTemp) {
-  currentTime = millis();                              // get current time
-  elapsedTime = (double)(currentTime - previousTime);  // compute time elapsed from previous computation
-
-  error = currentTemp - goalTemp;                 // determine error
-  cumError += error * elapsedTime;                // compute integral
-  rateError = (error - lastError) / elapsedTime;  // compute derivative
-
-  output = (kp * error) + (ki * cumError) + (kd * rateError);  // PID output
-
-  lastError = error;           // remember current error
-  previousTime = currentTime;  // remember current time
-}
-
 void setup() {
   Serial.begin(9600);
-  currentFaucet = 0;
+  curFaucet = 0;
   cumError = 0;
   lastError = 0;
   input = 0;
   output = 0;
   rateError = 0;
   error = 0;
-  previousTime = millis();
-  while (!Serial) {
-    delay(500);
-  }
+  goalTemp = 90;
+  prevTime = millis();
   hardwareInit();
-  Serial.println("Started.");
+  // manualControlMotor();
+}
+
+
+void computePID() {
+  elapsedTime = (double)(curTime - prevTime);  // compute time elapsed from previous computation
+
+  error = curTemp - goalTemp;                     // determine error
+  cumError += error * elapsedTime;                // compute integral
+  rateError = (error - lastError) / elapsedTime;  // compute derivative
+
+  output = (kp * error) + (ki * cumError) + (kd * rateError);  // PID output
+
+  lastError = error;   // remember current error
+  prevTime = curTime;  // remember current time
 }
 
 void loop() {
-  unsigned char currentTemp = getTemp();
-  computePID(currentTemp);
-  unsigned char goalFaucet = (unsigned char)constrain(output + currentFaucet, 0, maxFaucet);
-  setFaucet(goalFaucet);
-  currentFaucet = goalFaucet;
-  delay(samplePeriod);
+  curTime = millis();  // get current time
+  if (curTime > lastSampleTime + SAMPLE_PERIOD) {
+    curTemp = getTemp();
+    computePID();
+    goalFaucet = constrain(output + curFaucet, 0, MAX_FAUCET);
+    updateScreen(curTemp, goalTemp);
+    if (abs(goalFaucet - curFaucet) > OUPUT_NOISE_FILTER) {
+      curFaucet = goalFaucet;
+      setFaucet(goalFaucet);
+      lastMotorUpdateTime = curTime;
+    } else if (curTime > lastMotorUpdateTime + MOTOR_DISABLE_DELAY) {
+      setMotorEnable(false);  // Disable motor
+    }
+    lastSampleTime = curTime;
+  }
+  // Update servos
+  refreshServos();
+  delay(20);
 }
